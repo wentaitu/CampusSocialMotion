c76efa06383a4a2186f7357e0f1193c09f1b5302
liye12 <18875078710@163.com>
2017-11-06 00:41:59 +0800

    2
    2

--- app/src/main/java/cn/edu/cqupt/campussocialmotion/model/RedrockApiWrapper.java
+++ app/src/main/java/cn/edu/cqupt/campussocialmotion/model/RedrockApiWrapper.java
@@ -1,10 +1,53 @@
 package cn.edu.cqupt.campussocialmotion.model;
 
-public class RedrockApiWrapper<T> {
+import java.io.Serializable;
+
+public class RedrockApiWrapper<T> implements Serializable{
 
     public int status;
     public String info;
     public String version;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public T data;
     public long id;
 
